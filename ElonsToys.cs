cdfd443952e8128591ed7780765901fef893bf84
JOSE LUIS BENITEZ <joseluis.benitez@dxc.com>
2021-10-26 16:06:53 +0200

    primeros pasos en crear instancia

--- ElonsToys.cs
+++ ElonsToys.cs
@@ -1,15 +1,28 @@
 using System;
 
 class RemoteControlCar
+
 {
-    public static RemoteControlCar Buy()
+    private int _totalDistance;
+    private int _actualBattery;
+    public static RemoteControlCar Buy() => new RemoteControlCar();
+
+    public RemoteControlCar()
     {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Buy() method");
+        _totalDistance = 0;
+        _actualBattery = 100;
+    }
+    public RemoteControlCar(int distance, int battery)
+    {
+        _totalDistance = distance;
+        _actualBattery = battery;
     }
 
     public string DistanceDisplay()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDisplay() method");
+        
+        if (_totalDistance == 0) return "Driven 0 meters";
+        return string.Format("Driven {0} meters",_totalDistance);
     }
 
     public string BatteryDisplay()
